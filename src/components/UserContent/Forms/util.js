7f34f2fe9938a41ed5eb599581f85c8134782616
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-07-16 12:28:20 +0300

    Improved form validate logic. Fix validate form bug.

--- src/components/UserContent/Forms/util.js
+++ src/components/UserContent/Forms/util.js
@@ -8,8 +8,8 @@ export const validate = (values, props) => {
             errors.name = "Name should contain at least 5 characters!";
         } else if (!/[a-z]/gi.test(values.name)) {
             errors.name = "Name should contain only from characters!";
-        } else if (!/[A-Z][a-z]+/g.test(values.name)) {
-            errors.name = "The name must begin with a letter in the upper case!";            
+        } else if (!/^([A-Z][a-z]+)$/g.test(values.name)) {
+            errors.name = "The name must have only one letter in upper case, the one with which it begins.!";       
         }
     }
 
@@ -23,9 +23,11 @@ export const validate = (values, props) => {
         errors.password = "Password is not required!";
     } else if (values.password.length < 5) {
         errors.password = "Password should contain at least 5 characters!";
-    } else if (/[a-z]+/gi.test(values.password)) { // TO-DO: fix bug !!!
+    } else if (/^([a-z]+)$/gi.test(values.password)) {
         errors.password = "The password is not to contain only from characters!";
-    } else if (/\d+/g.test(values.password)) {
+    } else if (/\s+/g.test(values.password)) {
+        errors.password = "The password is can not include a space!";
+    } else if (/^([\d]+)$/g.test(values.password)) {
         errors.password = "The password is not to contain only from numbers!";
     } else if (!/[a-zA-Z0-9].+/g.test(values.password)) {
         errors.password = "The password is too weak! You can use characters in lowercase and upper case.";

d48521477315e8088971a4a471700d3abd710abd
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-01 12:44:35 +0300

    Improved RoomSchema component. Add SeatsInfo componennt.

--- src/components/UserContent/FilmCart/FilmCartContent/FilmCartContent.js
+++ src/components/UserContent/FilmCart/FilmCartContent/FilmCartContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import RoomSchema from "./RoomSchema/RoomSchema";
+import SeatsInfo from "./SeatsInfo/SeatsInfo";
 
 import "./FilmCartContent.css";
 
@@ -8,6 +9,7 @@ const FilmCartContent = ({ currentSeance }) => {
     return (
         <div className="seance-content">
             <RoomSchema currentSeance={currentSeance} />
+            <SeatsInfo />
         </div>
     );
 };
---
+++ src/components/UserContent/FilmCart/FilmCartContent/SeatsInfo/SeatsInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SeatsInfo.css";
+
+const SeatsInfo = () => {
+    return (
+        <div className="seance-content__info">
+            <div className="seance-content__info-title">Types of seats</div>
+            <div className="seance-content__info-list">
+                <div className="info-list__item">
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SeatsInfo;

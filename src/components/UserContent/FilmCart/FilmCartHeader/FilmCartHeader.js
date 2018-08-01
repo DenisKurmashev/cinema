7c6a7a02159b86b160ae0466ef0125c0a860e02e
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-01 10:04:29 +0300

    Improved FilmCartheader

--- src/components/UserContent/FilmCart/FilmCartHeader/FilmCartHeader.js
+++ src/components/UserContent/FilmCart/FilmCartHeader/FilmCartHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./FilmCartHeader.css";
 
-const FilmCartHeader = ({ currentSeance }) => {console.log(currentSeance);
+const FilmCartHeader = ({ currentSeance }) => {
     return (
         <div className="seance-header">
             <div className="seance-header__cover">
@@ -9,7 +9,12 @@ const FilmCartHeader = ({ currentSeance }) => {console.log(currentSeance);
             </div>
             <div className="seance-header__content">
                 <div className="seance-header__content-header">{currentSeance.film.name}</div>
-                <div className="seance-header__content-info"></div>
+                <div className="seance-header__content-info">
+                    <div>{currentSeance.cinema.city}</div>
+                    <div>{currentSeance.cinema.name}</div>
+                    <div>{(new Date(currentSeance.date)).toLocaleString()}</div>
+                    <div>Selected places: {currentSeance.selectedPlaces.length}</div>
+                </div>
             </div>
         </div>
     );

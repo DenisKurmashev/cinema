5de8756beb4eb8612d9f8413c75a11c0c9188e9a
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-03 12:15:30 +0300

    Add react-modal package. Add OrderModal compoment.

---
+++ src/components/UserContent/OrderModal/OrderModal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+class OrderModal extends React.PureComponent {
+
+    closeModal = () => {
+        // this.props.closeModalAction();
+    }
+
+    render() {
+        return (
+            <Modal
+                isOpen={this.props.isOpen}
+                onRequestClose={this.closeModal}
+            >
+                <h2>Modal</h2>
+            </Modal>
+        );
+    }
+
+}

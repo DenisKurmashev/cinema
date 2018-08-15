045e214531c6cfb79cd246201ff5209a95e7d982
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-15 12:55:38 +0300

    (AdminPage) Add basic CinemaLsit layout.

--- src/components/AdminContent/components/NewSeance/CinemasList/CinemasList.js
+++ src/components/AdminContent/components/NewSeance/CinemasList/CinemasList.js
@@ -6,8 +6,22 @@ import "./CinemasList.css";
 class CinemasList extends PureComponent {
 
     render() {
+        const { cinema, cinemaActions } = this.props;
+
         return (
-            <div></div>
+            <div className="cinema-content">
+                <div className="cinema-content-selected-item"></div>
+                <div className="cinema-content-list">
+                    {
+                        cinema.loadedCinemas.map((el, index) => (
+                            <div data-index={index} key={index} className={"list-item" + (index % 2 === 0 ? " gray" : "")}>
+                                <div>{el.name}</div>
+                                <div>{el.city}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         );
     }
 

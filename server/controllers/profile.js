d4905d32e839d29f4292cf890fdf6f41a3e68929
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-20 13:51:41 +0300

    Split profile validate service. Refactor profile controller. Fix register admin bug.

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -5,90 +5,79 @@ const User = require("../models/user");
 const config = require("../config.json");
 const errors = require("../helpers/errors");
 
-exports.login = async ctx => {
-    if (!ctx.request.body.email || !ctx.request.body.password) {
+const { validateLoginData, validateRegisterData } = require("../services/profile");
+
+const login = async ctx => {
+    const { status, error, data } = await validateLoginData(ctx.request.body);
+
+    if (!status) {
+        ctx.status = errors.wrongCredentials.status;
+        ctx.body = error.details;
+        return;
+    }
+
+    const { email, password } = data;
+
+    const user = await User
+        .findOne({ email })
+        .select("name email role created password")
+        .lean();
+
+    if (!user || !(await user.verify(password))) {
         ctx.status = errors.wrongCredentials.status;
         ctx.body = errors.wrongCredentials;
-    } else {
-        const validateSchema = joi.object().keys({
-            email: joi.string().email(),
-            password: joi.string().min(5).regex(/^[a-zA-Z0-9]{3,30}$/)
-        });
-        
-        try {
-            await joi.validate(ctx.request.body, validateSchema);
-        } catch(ex) {
-            console.log(ex);
-            ctx.status = errors.wrongCredentials.status;
-            return ctx.body = ex.details;
-        }
-
-        const { email, password } = ctx.request.body;
-
-        let user = await User.findOne({ email }).select("name email role created password");
-
-        if (!user || !(await user.verify(password))) {
-            ctx.status = errors.wrongCredentials.status;
-            ctx.body = errors.wrongCredentials;
-            return;
-        }
-
-        let userObject = {
-            id: user._id,
-            name: user.name,
-            email: user.email,
-            created: user.created,
-            role: user.role,
-        };
-
-        ctx.status = 200;
-        ctx.body = { user: userObject, token: "bearer " + jwt.sign({ id: user._id }, config.jwt) };
-
+        return;
     }
+
+    let userObject = {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        created: user.created,
+        role: user.role,
+    };
+
+    ctx.status = 200;
+    ctx.body = { user: userObject, token: "bearer " + jwt.sign({ id: user._id }, config.jwt) };
+
 };
 
-exports.register = async ctx => {
-    if (!ctx.request.body.name || !ctx.request.body.email || !ctx.request.body.password) {
+const register = async ctx => {
+    const { status, error, data } = await validateRegisterData(ctx.request.body);
+
+    if (!status) {
         ctx.status = errors.wrongCredentials.status;
-        ctx.body = errors.wrongCredentials;
-    } else {
-        const validateSchema = joi.object().keys({
-            name: joi.string().min(3),
-            email: joi.string().email(),
-            password: joi.string().min(5).regex(/^[a-zA-Z0-9]{3,30}$/)
-        });
-        
-        try {
-            await joi.validate(ctx.request.body, validateSchema);
-        } catch(ex) {
-            console.log(ex);
-            ctx.status = errors.wrongCredentials.status;
-            ctx.body = ex.details;
-            return;
-        }
-
-        let user; 
-        
-        try {
-            user = new User(ctx.request.body);
-            await user.save(); 
-        } catch(ex) {
-            console.log(ex);
-            ctx.status = errors.wrongCredentials.status;
-            ctx.body = ex;
-            return;
-        }
-
-        const userObject = {
-            id: user._id,
-            name: user.name,
-            email: user.email,
-            created: user.created,
-            role: user.role,
-        };
-        
-        ctx.status = 201;
-        ctx.body = { user: userObject, token: "bearer " + jwt.sign({ id: user._id }, config.jwt) };
-
+        ctx.body = error.details;
+        return;
     }
+
+    let user; 
+        
+    try {
+        user = new User(data);
+        await user.save(); 
+
+    } catch(ex) {
+        console.log(ex);
+        ctx.status = errors.wrongCredentials.status;
+        ctx.body = ex;
+        return;
+    }
+
+    const userObject = {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        created: user.created,
+        role: user.role,
+    };
+    
+    ctx.status = 201;
+    ctx.body = { user: userObject, token: "bearer " + jwt.sign({ id: user._id }, config.jwt) };
+
+};
+
+module.exports = {
+    login,
+    register,
 };
---
+++ server/services/profile.js
@@ -0,0 +1,58 @@
+const joi = require("joi");
+
+const validateLoginData = async (data) => {
+    let result = {
+        status: true,
+        error: null,
+        data,
+    };
+
+    const validateSchema = joi.object().keys({
+        email:    joi.string().email().required(),
+        password: joi.string().min(5).regex(/^[a-zA-Z0-9]{3,30}$/).required()
+    });
+
+    try {
+        await joi.validate(data, validateSchema);
+
+    } catch(ex) {
+        result.status = false;
+        result.error = ex;
+    }
+
+    return result;
+};
+
+const validateRegisterData = async (data) => {
+    let result = {
+        status: true,
+        error: null,
+        data,
+    };
+
+    const validateSchema = joi.object().keys({
+        name: joi.string().min(3).required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(5).regex(/^[a-zA-Z0-9]{3,30}$/).required()
+    });
+
+    try {
+        await joi.validate(data, validateSchema);
+
+    } catch(ex) {
+        result.status = false;
+        result.error = ex;
+    }
+
+    if (data.role) {
+        result.status = false;
+        result.error = "Validate role error!";
+    }
+
+    return result;
+};
+
+module.exports = {
+    validateLoginData,
+    validateRegisterData,
+};

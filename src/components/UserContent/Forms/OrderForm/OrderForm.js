e0d201f5acd8cb790f537cedbb77f4698d2d02bf
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-22 10:31:37 +0300

    Fix style bugs in congratulation block after ordering.

--- src/components/UserContent/Forms/OrderForm/OrderForm.js
+++ src/components/UserContent/Forms/OrderForm/OrderForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Additional from "./Additional/Additional";
 
+import "./OrderForm.css";
+
 class OrderForm extends React.PureComponent {
 
     componentWillUnmount() {
@@ -21,9 +23,11 @@ class OrderForm extends React.PureComponent {
                 {
                     order.isSuccess
                     ? (
-                        <div>
+                        <div className="default-form__congratulation" >
                             <div><h2>Congratulation!</h2></div>
-                            <div><input disabled={order.fetching} onClick={orderActions.onOrderFormClose} type="button" className="btn text-upper" value="close" /></div>
+                            <div className="close-btn-wrapper">
+                                <input disabled={order.fetching} onClick={orderActions.onOrderFormClose} type="button" className="btn text-upper" value="close" />
+                            </div>
                         </div>
                     )
                     : (

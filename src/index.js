678d1def27ef0491762f06cda32898f5f01fd711
Dzianis Kurmashou <Dzianis.Kurmashou@icx.local>
2018-08-22 13:21:36 +0300

    Split colors and indents css variables.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import routes from "./config/routes"; 
 
 import "./styles/colors.css";
+import "./styles/indent.css";
 import "./styles/button.css";
 import "./styles/form.css";
 import "./styles/common.css";

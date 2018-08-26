978b260f329618a8f874dc34a7501ce345d4fe85
Denis7 <deniskurmashev7@gmail.com>
2018-08-26 15:19:25 +0300

    Add actions test. Config tests tool.

---
+++ src/__tests__/actions/order.test.js
@@ -0,0 +1,42 @@
+import configStore from "redux-mock-store";
+
+import * as OrderActions from "../../actions/order";
+
+const mockStore = configStore();
+const store = mockStore();
+
+describe("Order actions", () => {
+    beforeEach(() => {
+        store.clearActions();
+    });
+});
+
+describe("Check all actions creators", () => {
+    test("Dispatch the current action and payload", () => {
+        const expectedAction = [
+            {
+                type: "ON_CHANGE_ORDER_SORT_FILTER",
+                payload: "",
+            },
+            {
+                type: "ON_ORDERS_FETCHING",
+                payload: "",
+            },
+            {
+                type: "ON_ORDERS_FAILED",
+                error: "",
+            },
+            {
+                type: "ON_ORDERS_SUCCESS",
+                orders: "",
+            },
+        ];
+
+        store.dispatch(OrderActions.onChangeOrderSortFilter(""));
+        store.dispatch(OrderActions.onOrdersFetching(""));
+        store.dispatch(OrderActions.onOrdersFailed(""));
+        store.dispatch(OrderActions.onOrdersSuccess(""));
+
+        expect(store.getActions()).toEqual(expectedAction);
+    });
+});
